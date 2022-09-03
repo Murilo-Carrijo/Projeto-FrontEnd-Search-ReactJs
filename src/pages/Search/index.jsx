774670ef9ed01ca82a3e1dccf0e9ea1bac462fe3
murilo <murilocarrijoadm@gmail.com>
2022-09-03 18:17:35 -0300

    feat: implementação do campo de busca na pagina Search

--- src/pages/Search/index.jsx
+++ src/pages/Search/index.jsx
@@ -1,9 +1,33 @@
 import React from 'react';
+import {
+  InputGroup, InputGroupText, Input, Button,
+} from 'reactstrap';
+import { IoIosSearch } from 'react-icons/io';
 import Header from '../../components/Header';
 
 function Search() {
   return (
-    <Header />
+    <div>
+      <Header />
+      <div className="container-input">
+        <InputGroup size="lg">
+          <InputGroupText>
+            <IoIosSearch />
+          </InputGroupText>
+          <Input
+            placeholder="Digite sua busca"
+            // onChange={(e) => setInputValue(e.target.value)}
+          />
+        </InputGroup>
+        <Button
+          type="button"
+          className="button-search"
+          // onClick={() => setSearch(inputValue)}
+        >
+          buscar
+        </Button>
+      </div>
+    </div>
   );
 }
 
