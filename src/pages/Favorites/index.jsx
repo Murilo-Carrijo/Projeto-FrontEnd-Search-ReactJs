da2c73fac3d12a52b77a8fa240c5495676a4c577
murilo <murilocarrijoadm@gmail.com>
2022-09-05 18:47:22 -0300

    feat: implementação da paginação para a tela de favoritos

--- src/pages/Favorites/index.jsx
+++ src/pages/Favorites/index.jsx
@@ -1,14 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Button } from 'reactstrap';
 import Header from '../../components/Header';
 import Cards from '../../components/Cards';
+import Pagination from '../../components/Pagination';
 import './Favorites.css';
+import '../Search/Search.css';
 
 function Favorites() {
+  const [ls, setLs] = useState([]);
+  const { actualPage, setActualPage } = Pagination();
+
+  useEffect(() => {
+    setLs(JSON.parse(localStorage.getItem('favorites')));
+  }, [ls]);
+
+  const pages = Math.ceil(ls.length / 10);
   return (
     <div>
       <Header />
       <div className="cards-container">
         <Cards />
+      </div>
+      <div className="pagination-container">
+        {pages > 0 && Array(pages).fill('').map((_, index) => (
+          <Button
+            className="button-pagination"
+            color="success"
+            type="button"
+            // eslint-disable-next-line react/no-array-index-key
+            key={index}
+            onClick={() => setActualPage(index + 1)}
+            // eslint-disable-next-line react/no-unknown-property
+            desabled={index === actualPage + 1}
+          >
+            { index + 1 }
+          </Button>
+        ))}
       </div>
     </div>
   );
