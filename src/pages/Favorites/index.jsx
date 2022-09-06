ff41c98b3599dc62563a2bafb2339ba28b8563c9
murilo <murilocarrijoadm@gmail.com>
2022-09-05 23:46:51 -0300

    fix: passando updateLs como parametro para o useEffect da pagina de favoritos

--- src/pages/Favorites/index.jsx
+++ src/pages/Favorites/index.jsx
@@ -17,7 +17,7 @@ function Favorites() {
       const articles = itemsLs.filter((_, i) => i <= 9);
       return setLs(articles);
     }
-    const min = (actualPage - 1 * 10);
+    const min = (actualPage - 1) * 10;
     const max = min + 9;
     const articles = itemsLs.filter((_, i) => i >= min && i <= max);
     return setLs(articles);
